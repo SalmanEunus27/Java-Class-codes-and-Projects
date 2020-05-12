8ad1037720ae2304f5d85259fe4b8c1cb78b1bdc
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-05-12 17:47:40 +0600

    Done

---
+++ Algo_Assignment1.java
@@ -0,0 +1,8 @@
+public class Algo_Assignment1 {
+    public static void main (String [] args){
+        long a =3141592653589793238462643383279502884197169399375105820 ;
+        long b =2718281828459045235360287471352662497757247093699959574;
+        System.out.println(a*b);
+    }
+    
+}
---
+++ Algorithm Assignment1.java
@@ -0,0 +1,8 @@
+public class Assignment1 {
+    public static void main (String [] args){
+        long a =3141592653589793238462643383279502884197169399375105820974944592 ;
+        long b =2718281828459045235360287471352662497757247093699959574966967627;
+        System.out.println(a*b);
+    }
+    
+}
